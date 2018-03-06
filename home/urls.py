60f9ec5110b06f8082c67f318fd7507f2ab6c028
Emmanuel Makonde <manutheitguy@gmail.com>
2018-03-06 16:46:16 +0300

    connecting the pages

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'about/$', views.about, name='about'),
-    url(r'services/$', views.services, name='services'),
     url(r'contact/$', views.contact, name='contact'),
+    url(r'trading/$', views.trading, name='trading'),
+    url(r'logistics/$', views.logistics, name='logistics'),
+    url(r'imports/$', views.imports, name='imports'),
+    url(r'exports/$', views.exports, name='exports'),
+    url(r'solutions/$', views.solutions, name='solutions'),
 ]
--- home/views.py
+++ home/views.py
@@ -8,8 +8,20 @@ def index(request):
 def about(request):
     return render(request, 'home/about.html')
 
-def services(request):
-    return render(request, 'home/services.html')
-
 def contact(request):
     return render(request, 'home/contact.html')
+
+def trading(request):
+    return render(request, 'home/trading.html')
+
+def logistics(request):
+    return render(request, 'home/logistics_list.html')
+
+def imports(request):
+    return render(request, 'home/imports_list.html')
+
+def exports(request):
+    return render(request, 'home/exports_list.html')
+
+def solutions(request):
+    return render(request, 'home/solutions_list.html')
